5659cf6bac278a5e4d79c5254f2fb8ee17f59e2a
10317 <10317@proton.me>
2025-02-18 20:57:51 -0600

    +   "41.neocities.org/sofia/sidx"

--- internal/internal.go
+++ internal/internal.go
@@ -2,6 +2,8 @@ package internal
 
 import (
    "41.neocities.org/dash"
+   "41.neocities.org/sofia/file"
+   "41.neocities.org/sofia/sidx"
    "41.neocities.org/widevine"
    xhttp "41.neocities.org/x/http"
    "bytes"
@@ -14,6 +16,140 @@ import (
    "os"
    "strings"
 )
+
+func write_sidx(req *http.Request, index dash.Range) ([]sidx.Reference, error) {
+   req.Header.Set("range", "bytes="+index.String())
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   data, err := io.ReadAll(resp.Body)
+   if err != nil {
+      return nil, err
+   }
+   var file1 file.File
+   err = file1.Read(data)
+   if err != nil {
+      return nil, err
+   }
+   return file1.Sidx.Reference, nil
+}
+
+var Forward = []struct {
+   Country string
+   Ip      string
+}{
+   {"Argentina", "186.128.0.0"},
+   {"Australia", "1.128.0.0"},
+   {"Bolivia", "179.58.0.0"},
+   {"Brazil", "179.192.0.0"},
+   {"Canada", "99.224.0.0"},
+   {"Chile", "191.112.0.0"},
+   {"Colombia", "181.128.0.0"},
+   {"Costa Rica", "201.192.0.0"},
+   {"Denmark", "2.104.0.0"},
+   {"Ecuador", "186.68.0.0"},
+   {"Egypt", "197.32.0.0"},
+   {"Germany", "53.0.0.0"},
+   {"Guatemala", "190.56.0.0"},
+   {"India", "106.192.0.0"},
+   {"Indonesia", "39.192.0.0"},
+   {"Ireland", "87.32.0.0"},
+   {"Italy", "79.0.0.0"},
+   {"Latvia", "78.84.0.0"},
+   {"Malaysia", "175.136.0.0"},
+   {"Mexico", "189.128.0.0"},
+   {"Netherlands", "145.160.0.0"},
+   {"New Zealand", "49.224.0.0"},
+   {"Norway", "88.88.0.0"},
+   {"Peru", "190.232.0.0"},
+   {"Russia", "95.24.0.0"},
+   {"South Africa", "105.0.0.0"},
+   {"South Korea", "175.192.0.0"},
+   {"Spain", "88.0.0.0"},
+   {"Sweden", "78.64.0.0"},
+   {"Taiwan", "120.96.0.0"},
+   {"United Kingdom", "25.0.0.0"},
+   {"Venezuela", "190.72.0.0"},
+}
+
+func write_segment(data, key []byte) ([]byte, error) {
+   if key == nil {
+      return data, nil
+   }
+   var file1 file.File
+   err := file1.Read(data)
+   if err != nil {
+      return nil, err
+   }
+   track := file1.Moof.Traf
+   if senc := track.Senc; senc != nil {
+      for i, data := range file1.Mdat.Data(&track) {
+         err = senc.Sample[i].DecryptCenc(data, key)
+         if err != nil {
+            return nil, err
+         }
+      }
+   }
+   return file1.Append(nil)
+}
+
+func segment_list(
+   client_id, private_key string, key_id, pssh1 []byte,
+   ext string,
+   license WidevineLicense,
+   represent *dash.Representation,
+) error {
+   file1, err := create(ext)
+   if err != nil {
+      return err
+   }
+   defer file1.Close()
+   initial, err := represent.SegmentList.Initialization.SourceUrl.Url(represent)
+   if err != nil {
+      return err
+   }
+   data, err := get(initial)
+   if err != nil {
+      return err
+   }
+   data, err = init_protect(data)
+   if err != nil {
+      return err
+   }
+   _, err = file1.Write(data)
+   if err != nil {
+      return err
+   }
+   key, err := get_key(client_id, private_key, key_id, pssh1, license)
+   if err != nil {
+      return err
+   }
+   http.DefaultClient.Transport = nil
+   var progress xhttp.ProgressParts
+   progress.Set(len(represent.SegmentList.SegmentUrl))
+   for _, segment := range represent.SegmentList.SegmentUrl {
+      media, err := segment.Media.Url(represent)
+      if err != nil {
+         return err
+      }
+      data, err := get(media)
+      if err != nil {
+         return err
+      }
+      progress.Next()
+      data, err = write_segment(data, key)
+      if err != nil {
+         return err
+      }
+      _, err = file1.Write(data)
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
 
 func get_key(
    client_id, private_key string, key_id, pssh []byte,
