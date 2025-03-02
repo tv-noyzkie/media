3bf98f7390c601c4c9be20cce232342d132b3d66
10317 <10317@proton.me>
2025-03-02 08:31:30 -0600

    deleted:    internal/plex/main.go

--- internal/plex/plex.go
+++ internal/plex/plex.go
@@ -4,10 +4,69 @@ import (
    "41.neocities.org/media/internal"
    "41.neocities.org/media/plex"
    "errors"
+   "flag"
    "fmt"
+   "os"
+   "path/filepath"
 )
 
+type flags struct {
+   address        plex.Address
+   e              internal.License
+   get_forward    bool
+   representation string
+   media string
+}
+
+func (f *flags) New() error {
+   var err error
+   f.media, err = os.UserHomeDir()
+   if err != nil {
+      return err
+   }
+   f.media = filepath.ToSlash(f.media) + "/media"
+   f.e.ClientId = f.media + "/client_id.bin"
+   f.e.PrivateKey = f.media + "/private_key.pem"
+   return nil
+}
+
+func main() {
+   var f flags
+   err := f.New()
+   if err != nil {
+      panic(err)
+   }
+   flag.Var(&f.address, "a", "address")
+   flag.StringVar(&f.e.ClientId, "c", f.e.ClientId, "client ID")
+   flag.StringVar(&f.representation, "i", "", "representation")
+   flag.StringVar(&f.e.PrivateKey, "p", f.e.PrivateKey, "private key")
+   flag.BoolVar(&f.get_forward, "g", false, "get forward")
+   flag.StringVar(&plex.ForwardedFor, "e", "", "set forward")
+   flag.Parse()
+   switch {
+   case f.get_forward:
+      for _, forward := range internal.Forward {
+         fmt.Println(forward.Country, forward.Ip)
+      }
+   case f.address[0] != "":
+      err := f.download()
+      if err != nil {
+         panic(err)
+      }
+   default:
+      flag.Usage()
+   }
+}
+
 func (f *flags) download() error {
+   if f.representation != "" {
+      f.e.Widevine = func(data []byte) ([]byte, error) {
+         return user.Widevine(part, data)
+      }
+      return f.e.Download(f.media + "/Mpd", f.representation)
+   }
+   // cache user
+   // cache metadata
    var user plex.User
    err := user.New()
    if err != nil {
@@ -21,28 +80,13 @@ func (f *flags) download() error {
    if err != nil {
       return err
    }
-   client, ok := metadata.Dash(user)
+   part, ok := metadata.Dash()
    if !ok {
-      return errors.New("Metadata.Dash")
+      return errors.New(".Dash()")
    }
-   represents, err := internal.Mpd(client)
+   resp, err := user.Mpd(part)
    if err != nil {
       return err
    }
-   for _, represent := range represents {
-      switch f.representation {
-      case "":
-         fmt.Print(&represent, "\n\n")
-      case represent.Id:
-         f.s.Client = client
-         return f.s.Download(&represent)
-      }
-   }
-   return nil
-}
-
-func get_forward() {
-   for _, forward := range internal.Forward {
-      fmt.Println(forward.Country, forward.Ip)
-   }
+   return internal.Mpd(f.media + "/Mpd", resp)
 }
--- plex/plex.go
+++ plex/plex.go
@@ -9,9 +9,50 @@ import (
    "strings"
 )
 
-var ForwardedFor string
+func (m *Metadata) Unmarshal(data Byte[Metadata]) error {
+   var value struct {
+      MediaContainer struct {
+         Metadata []Metadata
+      }
+   }
+   err := json.Unmarshal(data, &value)
+   if err != nil {
+      return err
+   }
+   *m = value.MediaContainer.Metadata[0]
+   return nil
+}
 
-func (u *User) New() error {
+type Metadata struct {
+   Media []struct {
+      Part []Part
+      Protocol string
+   }
+}
+
+func (u User) Metadata(match1 *Match) (Byte[Metadata], error) {
+   req, _ := http.NewRequest("", "https://vod.provider.plex.tv", nil)
+   req.URL.Path = "/library/metadata/" + match1.RatingKey
+   req.Header.Set("accept", "application/json")
+   req.Header.Set("x-plex-token", u.AuthToken)
+   if ForwardedFor != "" {
+      req.Header.Set("x-forwarded-for", ForwardedFor)
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+func (u *User) Unmarshal(data Byte[User]) error {
+   return json.Unmarshal(data, u)
+}
+
+type Byte[T any] []byte
+
+func NewUser() (Byte[User], error) {
    req, _ := http.NewRequest("POST", "https://plex.tv", nil)
    req.URL.Path = "/api/v2/users/anonymous"
    req.Header = http.Header{
@@ -21,11 +62,56 @@ func (u *User) New() error {
    }
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
-      return err
+      return nil, err
    }
    defer resp.Body.Close()
-   return json.NewDecoder(resp.Body).Decode(u)
+   return io.ReadAll(resp.Body)
 }
+
+func (u User) Widevine(part1 *Part, data []byte) ([]byte, error) {
+   req, err := http.NewRequest("POST", part1.License, bytes.NewReader(data))
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Scheme = "https"
+   req.URL.Host = "vod.provider.plex.tv"
+   req.URL.RawQuery = url.Values{
+      "x-plex-drm": {"widevine"},
+      "x-plex-token": {u.AuthToken},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+func (u User) Mpd(part1 *Part) (*http.Response, error) {
+   req, err := http.NewRequest("", part1.Key, nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Scheme = "https"
+   req.URL.Host = "vod.provider.plex.tv"
+   req.URL.RawQuery = "x-plex-token=" + u.AuthToken
+   req.Header = http.Header{}
+   if ForwardedFor != "" {
+      req.Header.Set("x-forwarded-for", ForwardedFor)
+   }
+   return http.DefaultClient.Do(req)
+}
+
+func (m *Metadata) Dash() (*Part, bool) {
+   for _, media := range m.Media {
+      if media.Protocol == "dash" {
+         return &media.Part[0], true
+      }
+   }
+   return nil, false
+}
+
+var ForwardedFor string
 
 type Address [1]string
 
@@ -69,47 +155,6 @@ type Match struct {
    RatingKey string
 }
 
-func (u User) Metadata(match1 *Match) (*Metadata, error) {
-   req, _ := http.NewRequest("", "https://vod.provider.plex.tv", nil)
-   req.URL.Path = "/library/metadata/" + match1.RatingKey
-   req.Header.Set("accept", "application/json")
-   req.Header.Set("x-plex-token", u.AuthToken)
-   if ForwardedFor != "" {
-      req.Header.Set("x-forwarded-for", ForwardedFor)
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var value struct {
-      MediaContainer struct {
-         Metadata []Metadata
-      }
-   }
-   err = json.NewDecoder(resp.Body).Decode(&value)
-   if err != nil {
-      return nil, err
-   }
-   return &value.MediaContainer.Metadata[0], nil
-}
-
-type Metadata struct {
-   Media []struct {
-      Part []Part
-      Protocol string
-   }
-}
-
-func (m *Metadata) Dash() (*Part, bool) {
-   for _, media := range m.Media {
-      if media.Protocol == "dash" {
-         return &media.Part[0], true
-      }
-   }
-   return nil, false
-}
-
 type Part struct {
    Key string
    License string
@@ -117,38 +162,4 @@ type Part struct {
 
 type User struct {
    AuthToken string
-}
-
-func (u User) Mpd(part1 *Part) (*http.Response, error) {
-   req, err := http.NewRequest("", part1.Key, nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Scheme = "https"
-   req.URL.Host = "vod.provider.plex.tv"
-   req.URL.RawQuery = "x-plex-token=" + u.AuthToken
-   req.Header = http.Header{}
-   if ForwardedFor != "" {
-      req.Header.Set("x-forwarded-for", ForwardedFor)
-   }
-   return http.DefaultClient.Do(req)
-}
-
-func (u User) Widevine(part1 *Part, data []byte) ([]byte, error) {
-   req, err := http.NewRequest("POST", part1.License, bytes.NewReader(data))
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Scheme = "https"
-   req.URL.Host = "vod.provider.plex.tv"
-   req.URL.RawQuery = url.Values{
-      "x-plex-drm": {"widevine"},
-      "x-plex-token": {u.AuthToken},
-   }.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
 }
